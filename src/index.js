0eb8827720d979ee854b90469ccd74fdcd5358f0
שגב סרור <segev@wisegroup.co.il>
2020-04-16 21:59:53 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import HomePage from "./temp/HomePage";
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route ptch={'/'}>
+        <HomePage/>
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/temp/HomePage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return <div>Home HomePage</div>;
+  }
+}
+export default HomePage;
