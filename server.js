3015f76922ab2d681ea8b1ab042a8726e3e37191
שגב סרור <segev@wisegroup.co.il>
2020-04-19 18:39:59 +0300

    change manifest

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
-
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
 });
-
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
