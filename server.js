246eb109d57dd7335d053be99e4e374d8e97dd44
שגב סרור <segev@wisegroup.co.il>
2020-04-19 16:58:11 +0300

    heroku test

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3000
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.listen(port, () => console.log("Listening on Port", port))
+const express = require('express');
+const app = express();
+// serve up production assets
+app.use(express.static('build'));
+// let the react app to handle any unknown routes
+// serve up the index.html if express does'nt recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+// if not in production use the port 5000
+const PORT = process.env.PORT || 5000;
+console.log('server started on port:',PORT);
+app.listen(PORT);
