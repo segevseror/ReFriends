f5507befeb12a3aa129432c9189462a812828f91
שגב סרור <segev@wisegroup.co.il>
2020-04-19 17:36:19 +0300

    ss

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
 const app = express();
-// serve up production assets
-app.use(express.static('build'));
-// let the react app to handle any unknown routes
-// serve up the index.html if express does'nt recognize the route
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
-// if not in production use the port 5000
-const PORT = process.env.PORT || 5000;
-console.log('server started on port:',PORT);
-app.listen(PORT);
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
