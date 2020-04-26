b7a5fcba2469de3fa74607ed425242277bb7c3b2
segev_wisegroup <segev@wisegroup.co.il>
2020-04-26 13:01:02 +0300

    change css

--- src/temp/CardMovies.js
+++ src/temp/CardMovies.js
@@ -1,68 +1,94 @@
 import {Col, Row} from "react-bootstrap";
 import React from "react";
+import {white} from "color-name";
+import styled from 'styled-components/macro'
 
 const CardMovies = props => {
-  const CardDetailesRow = props => {
-    const shereFriendsTitle = {
-      fontSize: "13px",
-      borderTop: "1px solid rgba(0,0,0,.125)",
-      paddingTop: "15px",
-      paddingBottom: "15px",
-      alignItems: "center"
+    const CardDetailesRow = props => {
+        const shereFriendsTitle = {
+            fontSize: "13px",
+            alignItems: "center"
+        };
+        var details = "";
+        if (Array.isArray(props.data)) {
+            details = props.data.map(function (value, key) {
+                return <span key={key}> {value}{(key != props.data.length - 1 ? ',' : '')} </span>;
+            });
+        } else {
+            details = props.data;
+        }
+        return (
+            <Col md={12}>
+                <Row style={shereFriendsTitle}>
+                    <Col className={"pr-0 col-auto"}>
+                        <b>{props.title}</b>
+                    </Col>
+                    <Col className={Array.isArray(props.data) ? 'text-right' : '"text-md-right text-left"'}>{details}</Col>
+                </Row>
+            </Col>
+        );
     };
-    var details = "";
-    if (Array.isArray(props.data)) {
-      details = props.data.map(function (value, key) {
-        return <span key={key}> {value}{(key != props.data.length - 1 ? ',' : '' )} </span>;
-      });
-    } else {
-      details = props.data;
+
+    const detailsbox = {
+        alignSelf: "flex-end",
+        paddingBottom: "15px",
+    };
+
+    const boxnovies = {
+        background: 'black',
+        color: 'white',
+        borderRadius: '5px',
+        overflow: 'hidden',
+        // border: '1px solid #e3e3e3',
+        boxShadow: 'rgba(255, 255, 255, 0.99) 0px 2px 8px',
+        height: '100%'
+    };
+
+    const promoStyle = {
+        fontSize: '13px',
+        color: '#79ebff'
+    };
+
+    const titleStyle = {
+        color: '#79ebff'
+    };
+    const ImgStyle = styled.div`
+    min-height: 431px;
+    @media screen and (max-width: 480px) {
+       min-height: 250px;
     }
+     `;
+
+
     return (
-      <Col md={12}>
-        <Row style={shereFriendsTitle}>
-          <Col className={"pr-0 col-auto"}>
-            <b>{props.title}</b>
-          </Col>
-          <Col className={"text-right"}>{details}</Col>
-        </Row>
-      </Col>
-    );
-  };
-
-  const detailsbox = {
-    alignSelf: "flex-end"
-  };
-
-  return (
-    <Col xl={2} md={3} xs={6} className={'mt-4'}>
-      <Row className={" boxnovies m-0"}>
-        <Col md={12}>
-          <Row className={'h-100'}>
-            <Col md={12} className={'p-0'}>
-              <img alt={'img'} src={props.img} className={"w-100"}/>
-            </Col>
-            <Col md={12} className={"mt-2"}>
-              <Row className={"h-100"}>
+        <Col xl={2} md={3} xs={6} className={'mt-4'}>
+            <Row className={"m-0"} style={boxnovies}>
                 <Col md={12}>
-                  <b>{props.title}</b>
+                    <Row className={'h-100'}>
+                        <ImgStyle md={12} className={'p-0'}>
+                            <img alt={'img'} src={props.img} className={"w-100"}/>
+                        </ImgStyle>
+                        <Col md={12} className={"mt-2"}>
+                            <Row className={"h-100"}>
+                                <Col md={12} style={titleStyle}>
+                                    <b>{props.title}</b>
+                                </Col>
+                                <Col md={12} style={promoStyle}>
+                                    <p>{props.promo}</p>
+                                </Col>
+                                <Col md={12} style={detailsbox}>
+                                    <Row>
+                                        <CardDetailesRow title={"sharing"} data={props.sharing}/>
+                                        <CardDetailesRow title={"Last Update"} data={props.cdate}/>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col md={12}>
-                  <p>{props.promo}</p>
-                </Col>
-                <Col md={12} style={detailsbox}>
-                  <Row>
-                    <CardDetailesRow title={"sharing"} data={props.sharing}/>
-                    <CardDetailesRow title={"Last Update"} data={props.cdate}/>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+            </Row>
         </Col>
-      </Row>
-    </Col>
-  );
+    );
 };
 
 export default CardMovies;
--- src/temp/Header.js
+++ src/temp/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col, Nav, Navbar} from "react-bootstrap";
+import {Col, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {logOutReducer} from "../actions";
@@ -49,20 +49,34 @@ const Header = (props) => {
   const dispatch = useDispatch();
   return (
     <Col xl={12} className={"p-0"}>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">ReFriends</Navbar.Brand>
-        <Nav className="mr-auto">
-          <NavLink exact className={"nav-link"} to="/">
-            Home
-          </NavLink>
-          <NavLink className={"nav-link"} to="/ref">
-            reference
-          </NavLink>
-          <NavLink exact className={"nav-link"} to="/myshare">
-            My shares
-          </NavLink>
-        </Nav>
-        <LoginBox dispatch={dispatch} isLogged={isLogged} />
+
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">ReFriends</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink exact className={"nav-link"} to="/">
+              Home
+            </NavLink>
+            <NavLink className={"nav-link"} to="/ref">
+              reference
+            </NavLink>
+            <NavLink exact className={"nav-link"} to="/myshare">
+              My shares
+            </NavLink>
+            {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
+            {/*  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Divider />*/}
+            {/*  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>*/}
+            {/*</NavDropdown>*/}
+          </Nav>
+          <Nav>
+            <LoginBox dispatch={dispatch} isLogged={isLogged} />
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </Col>
   );
