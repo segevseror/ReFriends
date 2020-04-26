fda9d92f656cec6b00f9187a40cbce6192747f8d
segev_wisegroup <segev@wisegroup.co.il>
2020-04-26 13:26:12 +0300

    fix css register

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
-  urlRequest: 'https://backend-api-segev.herokuapp.com',
+  urlRequest: 'http://localhost/backend',
   //localhost : http://netflixbackend.x
   //production url: https://backend-api-segev.herokuapp.com
 };
